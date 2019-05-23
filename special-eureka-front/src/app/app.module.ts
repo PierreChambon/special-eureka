35fcd3ad0cc197302a94f4897d807359f525093a
Pierre Chambon <pierre.chambon75@gmail.com>
2019-05-23 18:53:35 +0200

    Add: grid component

--- special-eureka-front/src/app/app.module.ts
+++ special-eureka-front/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'grid', component: GridComponent },
+  { path: '', component: GridComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
