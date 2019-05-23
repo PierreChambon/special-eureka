911e9c6bedc88067f158eb8844ea28478a575bf7
Arié Selinger <arie.selinger@gmail.com>
2019-05-23 19:11:15 +0200

    Update: app routing module

--- special-eureka-front/src/app/app.module.ts
+++ special-eureka-front/src/app/app.module.ts
@@ -5,23 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: 'grid', component: GridComponent },
-  { path: '', component: GridComponent }
-]
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent
-  ],
+  ], 
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
