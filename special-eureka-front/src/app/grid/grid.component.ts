e653152ff66b4667fccf29f653c3db28597e338c
Arié Selinger <arie.selinger@gmail.com>
2019-05-23 19:35:57 +0200

    Update: first grid and cell

--- special-eureka-front/src/app/grid/grid.component.ts
+++ special-eureka-front/src/app/grid/grid.component.ts
@@ -9,7 +9,24 @@ export class GridComponent implements OnInit {
 
   constructor() { }
 
+  number_rows = 5
+  number_cols = 5
+
+  grid;
+
   ngOnInit() {
+    this.initialize_grid()
+    console.log(this.grid)
+  }
+
+  initialize_grid(){
+    this.grid = []
+    for(let i=0; i<this.number_rows; i++){
+      this.grid[i] = []
+      for(let j=0; j<this.number_cols; j++){
+        this.grid[i][j] = -1
+      }
+    }
   }
 
 }
