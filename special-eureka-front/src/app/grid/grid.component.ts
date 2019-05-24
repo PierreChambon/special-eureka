3d1fd3b27a38a1a0872eda25b06df9239b1d2668
Arié Selinger <arie.selinger@gmail.com>
2019-05-24 18:19:29 +0200

    Update: grid component

--- special-eureka-front/src/app/grid/grid.component.ts
+++ special-eureka-front/src/app/grid/grid.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+class Cell {
+  value: number
+  isGiven: boolean
+  constructor(value: number = -1, isGiven : boolean = false){
+    this.value = value;
+    this.isGiven = isGiven
+  }
+}
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
+
 export class GridComponent implements OnInit {
 
   constructor() { }
 
   number_rows = 5
   number_cols = 5
-
   grid;
 
   ngOnInit() {
     this.initialize_grid()
-    console.log(this.grid)
+    this.grid[0][0].isGiven = true
   }
 
   initialize_grid(){
@@ -24,7 +33,7 @@ export class GridComponent implements OnInit {
     for(let i=0; i<this.number_rows; i++){
       this.grid[i] = []
       for(let j=0; j<this.number_cols; j++){
-        this.grid[i][j] = 5*i + j + 1
+        this.grid[i][j] = new Cell(5*i + j + 1)
       }
     }
   }
