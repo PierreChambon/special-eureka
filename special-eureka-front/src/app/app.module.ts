2ca1fbdf3791ee1e1e2a3c1b8980cfca858da464
Pierre Chambon <pierre.chambon75@gmail.com>
2019-06-02 18:46:40 +0200

    Add: new components for user sessions

--- special-eureka-front/src/app/app.module.ts
+++ special-eureka-front/src/app/app.module.ts
@@ -1,23 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { CellComponent } from './grid/cell/cell.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { HeaderComponent } from './header/header.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    CellComponent
+    CellComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent
   ], 
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
