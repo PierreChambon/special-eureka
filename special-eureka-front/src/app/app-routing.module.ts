3fe1f622815787753ed0593402d5dd677c68c15b
Pierre Chambon <pierre.chambon75@gmail.com>
2019-06-02 19:00:29 +0200

    Add: routing

--- special-eureka-front/src/app/app-routing.module.ts
+++ special-eureka-front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GridComponent } from './grid/grid.component'
+import { GridComponent } from './grid/grid.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+
 
 const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
   { path: 'grid', component: GridComponent },
   { path: '', component: GridComponent }
 ]
