aa25998d5f2c1e75efdabdf7c8e74c809c297551
Pierre Chambon <pierre.chambon75@gmail.com>
2019-06-02 19:10:53 +0200

    Add : link with a firebase database

--- special-eureka-front/src/app/app.component.ts
+++ special-eureka-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'special-eureka-front';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyDlhk8g3sr5vXuCI_Av0XHeI2eSX34xojo",
+      authDomain: "special-eureka.firebaseapp.com",
+      databaseURL: "https://special-eureka.firebaseio.com",
+      projectId: "special-eureka",
+      storageBucket: "special-eureka.appspot.com",
+      messagingSenderId: "1038588559422",
+      appId: "1:1038588559422:web:c01cf73b49fc230f"
+    };
+    firebase.initializeApp(config);
+  }
 }
