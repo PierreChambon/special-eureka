ea5bec0a4862c988ef22864b662ce0cdee1c9b60
Pierre Chambon <pierre.chambon75@gmail.com>
2019-06-02 20:26:15 +0200

    Update: authguard

--- special-eureka-front/src/app/app-routing.module.ts
+++ special-eureka-front/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { GridComponent } from './grid/grid.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   { path: 'auth/signup', component: SignupComponent },
   { path: 'auth/signin', component: SigninComponent },
-  { path: 'grid', component: GridComponent },
-  { path: '', component: GridComponent }
+  { path: 'grid', canActivate: [AuthGuardService], component: GridComponent },
+  { path: '', redirectTo: 'grid', pathMatch: 'full' },
+  { path: '**', redirectTo: 'grid' }
 ]
 
 @NgModule({
