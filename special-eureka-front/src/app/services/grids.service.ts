4d4f106abfff64fc3532e5e0e63bc8c420da4593
Pierre Chambon <pierre.chambon75@gmail.com>
2019-06-02 21:10:31 +0200

    Update: GridsService by creating a gridSubject, which deals with the interraction with the database

--- special-eureka-front/src/app/services/grids.service.ts
+++ special-eureka-front/src/app/services/grids.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Cell } from '../models/Cell';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GridsService {
 
+  grid: Cell[][];
+  gridSubject = new Subject<Cell[][]>();
   constructor() { }
+
+  emitGrid() {
+    this.gridSubject.next(this.grid);
+  }
 }
